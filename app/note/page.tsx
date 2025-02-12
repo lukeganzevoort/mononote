7ac8cda49ad3729c34ce76b15286447cc3e2e2d6
Luke Ganzevoort <98l75xr4@anonaddy.com>
2025-02-12 17:35:13 -0500

    added theme

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,16 +1,28 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/theme-toggle";
+
 export default function NotePage() {
   const note = { title: "Test", content: "test this note" };
 
   return (
-    <div className="h-screen w-screen">
-      <div className="h-full flex flex-col justify-center items-center space-y-2">
-        <div className="h-12 w-full flex items-center justify-between">
-          <span className="px-4">MONO.NOTE</span>
-          <span>{note.title}</span>
-          <span className="px-4 opacity-0">MONO.NOTE</span>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="h-screen w-screen font-mono">
+        <div className="h-full flex flex-col justify-center items-center space-y-2">
+          <div className="h-12 w-full flex items-center justify-between px-4">
+            <span>MONO.NOTE</span>
+            <span className="absolute left-1/2 transform -translate-x-1/2 font-mono">
+              {note.title}
+            </span>
+            <ModeToggle />
+          </div>
+          <div className="flex-grow"></div>
         </div>
-        <div className="flex-grow"></div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
