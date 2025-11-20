95d638d2447b1f346b82b3e70deb2985f9f36ff8
Luke Ganzevoort <98l75xr4@anonaddy.com>
2025-11-20 15:21:26 -0500

    Refactor note links to use query parameters for note ID and demo mode

--- components/nav-top.tsx
+++ components/nav-top.tsx
@@ -3,16 +3,17 @@
 import { ModeToggle } from "./theme-toggle";
 import { SidebarTrigger } from "./ui/sidebar";
 import { UserMenu } from "./user-menu";
-import { useParams, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
 import pb from "@/lib/pocketbase";
 import { DEMO_NOTES } from "@/pocketbase/demo_data";
+import { usePathname } from "next/navigation";
 
 export const NavTop = () => {
-  const params = useParams<{ noteId: string | undefined }>();
   const searchParams = useSearchParams();
   const isDemoMode = searchParams.get("demo") === "true";
-
+  const noteId = searchParams.get("id");
+  const pathname = usePathname();
   // Always call useSWR to maintain hook order
   const { data: notes } = useSWR(["notes", isDemoMode], async () => {
     if (isDemoMode) {
@@ -25,9 +26,8 @@ export const NavTop = () => {
   });
 
   let noteTitle = "All Notes";
-  if (params.noteId) {
-    const currentNoteId = params.noteId;
-    const currentNote = notes?.find((note) => note.id === currentNoteId);
+  if (pathname.includes("/app/note")) {
+    const currentNote = notes?.find((note) => note.id === noteId);
     noteTitle = currentNote?.title ?? "Untitled Note";
   }
 
