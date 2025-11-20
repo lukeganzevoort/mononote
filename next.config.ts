17387943e1aa552e97b0674d393e0f683ceee3c5
Luke Ganzevoort <98l75xr4@anonaddy.com>
2025-11-20 15:39:25 -0500

    fix trailing slash

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
 };
 
 export default nextConfig;
