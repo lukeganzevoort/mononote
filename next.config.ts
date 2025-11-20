be17db18f0bea92611f0b8362418180c812e9ba0
Luke Ganzevoort <98l75xr4@anonaddy.com>
2025-11-20 15:57:20 -0500

    no trailing slash

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   output: "export",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,
+  trailingSlash: false,
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
