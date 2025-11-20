a6d35c99d21d7983d8e15df2e53274f4b33a060b
Luke Ganzevoort <98l75xr4@anonaddy.com>
2025-11-20 16:18:01 -0500

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: false,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  // Important: This ensures client-side navigation works
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
